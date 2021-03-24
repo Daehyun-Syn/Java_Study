8ab48a7740946773e23de787d080ae049ae7c4bb
data15 <data15@DESKTOP-JVD8Q7N>
2021-03-24 11:04:45 +0900

    If test1

---
+++ DhsynPrj/src/Test4.java
@@ -0,0 +1,15 @@
+
+public class Test4 {
+
+	public static void main(String[] args) {
+int a = 100;
+
+if (a < 100) {
+	System.out.println("100 보다 작군요");
+}
+		System.out.println("난 무조건 실행된다.");
+		
+	
+	}
+
+}
