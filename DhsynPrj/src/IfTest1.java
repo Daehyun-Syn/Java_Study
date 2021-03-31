a26de92162b91c128979362c7047709770ab88aa
data15 <data15@DESKTOP-JVD8Q7N>
2021-03-31 10:07:25 +0900

    ifTest1

---
+++ DhsynPrj/src/IfTest1.java
@@ -0,0 +1,28 @@
+
+public class IfTest1 {
+
+	public static void main(String[] args) {
+
+		int a = 200;
+		
+		if(a<100) {
+			System.out.println("100보다 작군요..");
+		    System.out.println("참이면 보이죠?");
+		} else {
+			System.out.printf("100보다 크군요.. \n");
+            System.out.println("거짓이면 보이죠?");		
+		
+		}
+		
+		System.out.println("난 조건절에 상관없이 무조건 보인다.");
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
